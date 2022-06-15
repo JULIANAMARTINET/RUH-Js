5480c867413719b123a846e7909b0c11ce2215fc
JULIANAMARTINET <martinetjuliana@gmail.com>
2022-06-15 19:36:59 +0200

    1era entrega proyecto Final

--- js/script.js
+++ js/script.js
@@ -3,22 +3,6 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("down", window.scrollY > 0);
 });
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   new Splide('#thumbnail-carousel', {
-//     //   fixedWidth : 200,
-//     gap: 3,
-//     rewind: true,
-//     pagination: false,
-//     perMove: 1,
-//     type: 'loop',
-//     focus: 2 | 'center',
-//     perPage: 4,
-//     speed: 400,
-//     autoplay: true,
-//     interval: 3000
-//   }).mount();
-// });
-
 // -- Desafio --
 
 // Inicio de sesion
@@ -45,60 +29,48 @@ for (i = 2; i >= 1; i--) {
 if (contraseña != 1234)
   alert("Ha superado la cantidad de intentos, su cuenta ha sido bloqueada");
 
-//    Detalle de productos
-//  class Producto {
-//    constructor (nombre, precio) {
-//      this.nombre = nombre.toUpperCase();
-//      this.precio = parceFloat(precio);
-//    }
-//  }
-//  const productos = [];
-//  productos.push (new Producto("VELAS AROMATICAS", 60));
-//  productos.push (new Producto("SAHUMERIO", 20));
+// Carrito de compras 
 
-
-
-// Carrito de compras
-
-let acumulador = 0;
-
-function carrito() {
-
-  switch (producto) {
-    case "VELAS AROMATICAS":
-      precioCompra = cantidad * 60;
-      acumulador += precioCompra;
-      return alert("El total de su compra es de $" + acumulador);
-    case "SAHUMERIOS":
-      precioCompra = cantidad * 20;
-      acumulador += precioCompra;
-      return alert("El total de su compra es de $ " + acumulador);
-    default:
-      return alert("operacion incorrecta");
+class Productos {
+  constructor(id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre.toUpperCase();
+    this.precio = Number(precio);
+  }
+  precioIva() {
+    this.precio *= 1.21
   }
 }
 
-let productos = [
-  {nombre: "Velas Aromaticas", precio: 60},
-  {nombre: "Sahumerios", precio: 20}
-]
+const productos = [];
+let producto;
+let total = 0
+let carrito = []
+productos.push(new Productos(1, "VELAS AROMATICAS", 60));
+productos.push(new Productos(2, "SAHUMERIOS", 20));
+productos.push(new Productos(3, "SAHUMOS", 30));
+productos.push(new Productos(4, "DEFUMADORES", 40));
+
+productos.filter(el => {
+  el.precioIva()
+});
+
+function compra(pregunta) {
+  carrito.push(productos.find(el => el.id == pregunta));
+}
+
+function totalCarrito() {
+  total = carrito.reduce((acc, el) => acc + el.precio, 0).toFixed(2);
+}
 
 while (true) {
-  let nombres = productos.map(elemento => elemento.nombre);
-
-  producto = prompt(
-    "¿Qué producto te gustaria agregar al carrito? \n" + nombres.join(" \n ")).toUpperCase();
-  cantidad = prompt("¿Qué cantidad de " + producto +  " queres?");
-  carrito(producto);
-  let exit = prompt(
-    "¿Quieres seguir comprando? \n Escriba \n SI - Para continuar \n ESC - Para finalizar la compra"
-  ).toUpperCase();
-  if (exit == "ESC") {
+  let pregunta = prompt("¿Qué producto te gustaria agregar al carrito? \n (Ingresa el numero correspondiente al producto \n \n 1- Velas Aromaticas \n 2- Sahumerios \n 3- Sahumos \n 4- Defumadores \n Escriba ESC para finalizar compra").toUpperCase();
+  if (pregunta == "ESC") {
+    totalCarrito();
+    console.log(carrito)
+    alert(`Su compra Total es de ${total}`)
     break;
+  } else {
+    compra(Number(pregunta))
   }
 }
-
-
-
-
- 
