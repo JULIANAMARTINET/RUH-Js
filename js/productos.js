e4962118d792f2e66e8d7a7e639329365c03924a
JULIANAMARTINET <martinetjuliana@gmail.com>
2022-06-21 17:08:58 +0200

    Carrito de compras

--- js/productos.js
+++ js/productos.js
@@ -1,45 +1,148 @@
-// Carrito de compras 
+class Producto {
+  constructor(id, nombre, precio, img) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.img = img;
+    this.cantidad = 1;
+    this.inPortfolio = false;
+  }
+  addToPorfolio() {
+    this.cantidad++;
+    this.inPortfolio = true;
+  }
+  actualizarPrecioTotal() {
+    this.totalPrecio = this.precio * this.cantidad;
+  }
+}
 
-class Productos {
-    constructor(id, nombre, precio) {
-      this.id = id;
-      this.nombre = nombre.toUpperCase();
-      this.precio = Number(precio);
-    }
-    precioIva() {
-      this.precio *= 1.21
-    }
+// array de stock de productos
+const stockProductos = [];
+
+//Pusheo los productos al Stock
+stockProductos.push(new Producto("01", "Linea Palo Santo", 400, '../img/Prod-Palo-Santo.png'));
+stockProductos.push(new Producto("02", "Linea Natural", 450, '../img/prod-s-natural.png'));
+stockProductos.push(new Producto("03", "Linea Premium", 500, '../img/prod-s-premium.png'));
+stockProductos.push(new Producto("04", "Porta Sahumerio Madera", 250, '../img/prod-porta-shau.png'));
+stockProductos.push(new Producto("05", "Ramillo de Sahumo", 500, '../img/prod-sahumo-1.png'));
+stockProductos.push(new Producto("06", "Pastillas Defumadoras", 600, '../img/prod-Pastillas.png'));
+stockProductos.push(new Producto("07", "Piramides Defumadoras", 600, '../img/prod-piramide.png'));
+stockProductos.push(new Producto("08", "Bombitas Defumadoras", 600, '../img/prod-bombitas.png'));
+stockProductos.push(new Producto("09", "Bombitas 7 Chakras", 700, '../img/prod-siete-chacras.png'));
+stockProductos.push(new Producto("10", "Sahumador de Ceramica", 1200, '../img/prod-sahumadores.png'));
+stockProductos.push(new Producto("11", "Vela Con Flores", 800, '../img/prod-velas.png'));
+stockProductos.push(new Producto("12", "Mini Velas Madera", 300, '../img/prod-velitas.png'));
+
+
+const contenedorProductos = document.getElementById("contenedor-productos")
+const contenedorCarrito = document.getElementById("carrito-contenedor")
+const contadorCarrito = document.getElementById('contadorCarrito')
+const vaciarCarrito = document.getElementById('vaciarCarrito')
+const pagarCarrito = document.getElementById('pagarCarrito')
+const totalPrecioCarrito = document.getElementById('totalPrecioCarrito')
+const cantidadTotal = document.getElementById('cantidadTotal')
+
+let carrito = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+    actualizarCarrito()
   }
-  
-  const productos = [];
-  let producto;
-  let total = 0
-  let carrito = []
-  productos.push(new Productos(1, "VELAS AROMATICAS", 60));
-  productos.push(new Productos(2, "SAHUMERIOS", 20));
-  productos.push(new Productos(3, "SAHUMOS", 30));
-  productos.push(new Productos(4, "DEFUMADORES", 40));
-  
-  productos.filter(el => {
-    el.precioIva()
-  });
-  
-  function compra(pregunta) {
-    carrito.push(productos.find(el => el.id == pregunta));
+})
+
+stockProductos.forEach((producto) => {
+  let card = document.createElement("div")
+  card.innerHTML = `
+  <figure class="card m-4">
+        <img src="${producto.img}" class="card-img-top" alt="${producto.nombre}">
+        <p class="card-title">${producto.nombre} </p>
+        <p class="card-text">$ ${producto.precio}</p>
+        <button class="btn btn-primary" id="p${producto.id}">Agregar al Carrito</button>
+  </figure> 
+  `
+  contenedorProductos.appendChild(card);
+
+  const boton = document.getElementById(`p${producto.id}`)
+  boton.addEventListener("click", () => {
+    agregarAlCarrito(producto.id)
+  })
+
+})
+
+// Funcion argegar al carrito
+
+const agregarAlCarrito = (productoId) => {
+  const item = carrito.find((producto) => producto.id === productoId);
+  if (item) {
+    let index = carrito.findIndex((producto) => producto.id === item.id);
+    carrito[index].addToPorfolio();
+    carrito[index].actualizarPrecioTotal();
+  } else {
+    let newProducto = stockProductos.find((producto) => producto.id === productoId);
+    carrito.push(newProducto);
+    carrito[carrito.length - 1].actualizarPrecioTotal();
+
   }
-  
-  function totalCarrito() {
-    total = carrito.reduce((acc, el) => acc + el.precio, 0).toFixed(2);
-  }
-  
-  while (true) {
-    let pregunta = prompt("¿Qué producto te gustaria agregar al carrito? \n (Ingresa el numero correspondiente al producto \n \n 1- Velas Aromaticas \n 2- Sahumerios \n 3- Sahumos \n 4- Defumadores \n Escriba ESC para finalizar compra").toUpperCase();
-    if (pregunta == "ESC") {
-      totalCarrito();
-      console.log(carrito)
-      alert(`Su compra Total es de ${total}`)
-      break;
-    } else {
-      compra(Number(pregunta))
-    }
-  }
+  actualizarCarrito()
+}
+
+const deleteCart = (productoId) => {
+  const item = carrito.find((producto) => producto.id === productoId);
+  const index = carrito.indexOf(item);
+  carrito.splice(index, 1);
+  actualizarCarrito()
+}
+
+vaciarCarrito.addEventListener('click', () => {
+  carrito.length = 0
+  actualizarCarrito()
+})
+
+// Funcion pagar carrito
+pagarCarrito.addEventListener('click', () => {
+  alert(`Tu compra total es de $${totalCarrito}. Gracias! 😄`);
+  carrito.length = 0;
+  actualizarCarrito()
+})
+
+const actualizarCarrito = () => {
+  contenedorCarrito.innerHTML = ""
+
+  carrito.forEach((producto) => {
+    let card = document.createElement("div")
+    card.innerHTML = `
+    <figure class="card mb-4">
+      <div class="row g-0">
+          <div class="col-md-3 img-carrito">
+              <img src="${producto.img}" class="img-fluid rounded-start" alt="${producto.nombre}">
+          </div>
+          <div class="col-md-6">
+             <div class="card-detalle">
+                <p class="card-title">${producto.nombre} </p>
+                <p class="card-text">Cant: ${producto.cantidad}</p>
+                <p class="card-text">Total: $ ${producto.totalPrecio}</p>
+             </div>   
+          </div>
+          <div class="col-md-3 d-flex">
+                <button class="btn btn-primary eliminar" id="eliminar${producto.id}">Eliminar</button>
+          </div>
+      </div>
+    </figure  
+    `
+
+    contenedorCarrito.appendChild(card);
+
+    const botonDelete = document.getElementById(`eliminar${producto.id}`)
+    botonDelete.addEventListener('click', () => {
+      deleteCart(producto.id)
+    })
+  })
+
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+
+  contadorCarrito.innerText = carrito.length
+
+  totalPrecioCarrito.innerText = carrito.reduce((total, elemento) => total + elemento.totalPrecio, 0);
+  totalCarrito = carrito.reduce((total, elemento) => total + elemento.totalPrecio, 0);
+}
--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
-window.addEventListener("scroll", function () {
-  let header = document.querySelector("#header-home");
-  header.classList.toggle("down", window.scrollY > 0);
-});
+ window.addEventListener("scroll", function () {
+   let header = document.querySelector("#header-home");
+   header.classList.toggle("down", window.scrollY > 0);
+ })
+ 
+// // -- Desafio --
 
-// -- Desafio --
+// // Inicio de sesion
 
-// Inicio de sesion
+// let inicio = document.getElementById("inicio")
+// inicio.onclick = () => {
 
-let usuario = prompt("Ingrese su nombre de usuario");
-let contraseña = " ";
+// let usuario = prompt("Ingrese su nombre de usuario");
+// let contraseña = " ";
 
-while (usuario.toUpperCase() != "JULIANA") {
-  alert("nombre de usuario incorrecto, vuelve a intentarlo");
-  usuario = prompt("Ingrese su nombre de usuario");
-}
+// while (usuario.toUpperCase() != "JULIANA") {
+//   alert("nombre de usuario incorrecto, vuelve a intentarlo");
+//   usuario = prompt("Ingrese su nombre de usuario");
+// }
 
-contraseña = prompt("Hola " + usuario + " Ingrese su contraseña");
+// contraseña = prompt("Hola " + usuario + " Ingrese su contraseña");
 
-for (i = 2; i >= 1; i--) {
-  if (contraseña == 1234) {
-    alert("Bienvenido/a " + usuario);
-    break;
-  }
-  alert("contraseña incorrecta, le quedan " + i + " intentos");
-  contraseña = prompt("Hola " + usuario + " Ingrese su contraseña");
-}
+// for (i = 2; i >= 1; i--) {
+//   if (contraseña == 1234) {
+//     alert("Bienvenido/a " + usuario);
+//     break;
+//   }
+//   alert("contraseña incorrecta, le quedan " + i + " intentos");
+//   contraseña = prompt("Hola " + usuario + " Ingrese su contraseña");
+// }
 
-if (contraseña != 1234)
-  alert("Ha superado la cantidad de intentos, su cuenta ha sido bloqueada");
+// if (contraseña != 1234)
+//   alert("Ha superado la cantidad de intentos, su cuenta ha sido bloqueada");
+// }
+
+
+
+// let usuario; 
+
+// let usuarioStorage = localStorage.getItem("user");
+
+// if(usuarioStorage) {  
+//   let message =  `Bienvenido ${usuarioStorage}`;
+//   alert(message);
+//   usuario = usuarioStorage
+
+// }else {
+//   alert("no estas en seson")
+//   user = prompt("ingrese su nombre")
+//   localStorage.getItem("user", user)
+// }
+
 
